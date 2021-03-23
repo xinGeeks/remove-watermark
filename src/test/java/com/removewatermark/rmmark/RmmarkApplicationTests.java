f520672c3e4cf812b80affccb7028a8a46ec6738
Semper_FI <xzhaoGeeks@gmail.com>
2021-03-23 23:20:11 +0800

    快手，抖音，皮皮虾，火山

---
+++ src/test/java/com/removewatermark/rmmark/RmmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.removewatermark.rmmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmmarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
